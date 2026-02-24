c4ec9595af382683c0bd36106e275096e80bd01f
Amanda Duarte <amandaduuaarte@gmail.com>
2026-02-24 20:48:01 -0300

    fix: ajusting app lister port

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get(/^\/(?!api).*/, (req, res) => {
 // =========================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
